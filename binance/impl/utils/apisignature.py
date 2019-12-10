045aec26cf3773273d94755ab2ad9ff554926525
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-12-10 20:52:18 +0300

    delete ch in apisignature

--- binance/impl/utils/apisignature.py
+++ binance/impl/utils/apisignature.py
@@ -11,13 +11,9 @@ def create_signature(secret_key, builder):
     if secret_key is None or secret_key == "":
         raise BinanceApiException(BinanceApiException.KEY_MISSING,  "Secret key are required")
 
-    # 对参数进行排序:
     keys = builder.param_map.keys()
-    # 加入&
     query_string = '&'.join(['%s=%s' % (key, parse.quote(builder.param_map[key], safe='')) for key in keys])
-
     signature = hmac.new(secret_key.encode(), msg=query_string.encode(), digestmod=hashlib.sha256).hexdigest()
-    
     builder.put_url("signature", signature)
 
 
