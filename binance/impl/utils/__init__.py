a7ab885814e4df6c4a475ce0f33ddfff27502243
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-12-13 13:55:54 +0300

    fix wrong boolean json

--- binance/impl/utils/__init__.py
+++ binance/impl/utils/__init__.py
@@ -3,4 +3,6 @@ from binance.impl.utils.jsonwrapper import JsonWrapper
 
 
 def parse_json_from_string(value):
+    value = value.replace("False","false")
+    value = value.replace("True","true")
     return JsonWrapper(json.loads(value))
